c1f09095456cc4fc48debd6acf8cd27324fc6b59
Jake Burton <jburto94@yahoo.com>
2022-06-16 11:09:48 -0400

    added user validations

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,20 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  const existingUser = await User.findOne({ username })
+
+  if (!password || password.length < 3) {
+    return response.status(400).json({
+      error: 'valid password must be provided'
+    })
+  }
+
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username already registered'
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
